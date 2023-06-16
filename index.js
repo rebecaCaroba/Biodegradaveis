c3926e4925779afa6afef7847127cfbb9bf80a73
rebecaCaroba <rebecacaroba@gmail.com>
2023-06-15 21:51:05 -0300

    Adicionado narrador de texto

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+
+let utterance;
+
+function speakText() {
+    const text = document.querySelector('#texta');
+    const textDiv = text.innerText;
+
+  if (utterance && !utterance.paused) {
+    // Se já estiver falando, não faça nada
+    return;
+  }
+
+  utterance = new SpeechSynthesisUtterance(textDiv);
+  speechSynthesis.speak(utterance);
+}
+
+function stopText() {
+  if (utterance) {
+    // Se houver uma fala em andamento, pare a fala
+    speechSynthesis.cancel();
+  }
+}
+
+
+// function speakText() {
+//     const text = document.querySelector('#texta');
+//     const textDiv = text.innerText;
+    
+//     let utterance = new
+//     SpeechSynthesisUtterance(textDiv);
+//     speechSynthesis.speak(utterance)
+
+// }
+
 function toggleMenu() {
     const nav = document.querySelector('#nav');
     nav.classList.toggle('active')
